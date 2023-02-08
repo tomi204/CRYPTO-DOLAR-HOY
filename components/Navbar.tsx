b1988d391b503bdf16095cc2230359bf4bcb329b
tomas <tomi204@outlook.com>
2023-02-08 19:18:44 -0300

    delete connect

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,6 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import React from "react";
 
 const Navbar = () => {
-  return (
-    <nav className="bg-gray-800 w-full mx-auto">
-      <ConnectButton />
-    </nav>
-  );
+  return <nav className="bg-gray-800 w-full mx-auto h-16"></nav>;
 };
 export default Navbar;
